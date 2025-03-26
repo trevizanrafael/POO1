a619e47f1aac9cdeb0a43c501329f3bdc4ee6d75
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-03-26 00:49:56 -0300

    Create SaidaDadosCadastrais.java

---
+++ SaidaDadosCadastrais.java
@@ -0,0 +1,10 @@
+public class SaidaDadosCadastrais{
+	public static void main (String arg[]){
+		System.out.println("Nome:");
+		String nome = "Rafael Moraes Trevizan";
+    System.out.println(nome);
+    System.out.println("RA:");
+    int ra = 2648938;
+    System.out.println(ra);
+	}
+}
