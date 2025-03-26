a04a1ccf16e5ea84bd5ac561bdd31ee002e17ad5
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-03-26 00:41:32 -0300

    Create Saida.java

---
+++ Saida.java
@@ -0,0 +1,7 @@
+//Exercício a.1)
+public class Saida{
+	public static void main (String arg[]){
+		System.out.println(“Facim, facim!!!");
+		System.out.println(“\nSegunda Linha: Facim, facim!!!");
+	}
+}
