ab93a6e494bbf85c725bc239a9fe56b8b35f5a80
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-03-26 15:14:07 -0300

    Create Laco2.java

---
+++ Laco2.java
@@ -0,0 +1,10 @@
+//Exercício d.1)
+public class Laco2 {
+  public static void main(String arg[]){
+    int a=0, b=15;
+    while(b>=a){
+    System.out.println("O Valor de B dentro do while é: "+b);
+    b--;
+    }
+  }
+}
