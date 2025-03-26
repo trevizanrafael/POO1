3ac7b74200cf09ed16778bd281609dbb6a8f4677
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-03-26 15:19:53 -0300

    Create Laco3.java

---
+++ Laco3.java
@@ -0,0 +1,11 @@
+//Exercício e.1)
+public class Laco3 {
+  public static void main(String arg[]){
+    int a=0, b=15;
+    do{
+    System.out.println("O valor de A dentro do DO é: "+b);
+    b--;
+    }while(b>=a);
+    System.out.println("");
+  }
+}
