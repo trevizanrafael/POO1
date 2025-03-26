d062358fa0862e5977919d6b942006b005d4a591
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-03-26 15:11:09 -0300

    Create Laco1.java

---
+++ Laco1.java
@@ -0,0 +1,10 @@
+//Exercicio c.1)
+public class Laco1 {
+  public static void main(String arg[]){
+    int a=0;
+    int b=10;
+    for(b=10; b>=a; b--){
+      System.out.println("O Valor de B dentro do FOR é: "+b);
+    }
+    }
+}
