3bd2369a955b3de67b385981927b7531a9a67a23
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-03-26 15:32:16 -0300

    Create Selec1.java

---
+++ Selec1.java
@@ -0,0 +1,13 @@
+//Exercício f.1)
+public class Selec1 {
+  public static void main(String arg[]){
+    int a=0, b=20;
+    if(a>b){
+    System.out.println("A e maior que b");
+    } else if (b>a){
+    System.out.println("A e menor que b");    
+    } else {
+    System.out.println("A e igual a b");
+    }
+  }
+}
