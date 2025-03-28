3e404da67eb3e729d57cdd3067ac8981c50650a3
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-03-28 19:02:26 -0300

    Delete Saida.java

--- Saida.java
+++
@@ -1,7 +0,0 @@
-//Exercício a.1)
-public class Saida{
-	public static void main (String arg[]){
-		System.out.println(“Facim, facim!!!");
-		System.out.println(“\nSegunda Linha: Facim, facim!!!");
-	}
-}
