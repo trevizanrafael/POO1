eb1920ded3bfc255b28cb4a2dca8d6537444afb0
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-04-01 20:03:21 -0300

    Create calcular.java

---
+++ calcular.java
@@ -0,0 +1,12 @@
+public class Calcular{
+    
+	public static void main (String args[]){
+  
+		int z = Integer.parseInt(args[0]), w = Integer.parseInt(args[1]);
+		System.out.println("Valor de Z:\n"+z+"\nValor de w:\n"+w+"\nSoma de z e w:\n"+(z+w)+"\nSubtracao de z e w:
+
+\n"+(z-w)+"\nMultiplicacao de z e w:\n"+(z*w)+"\nDivisao de z e w:\n"+(z/w));
+	}
+
+
+} 
