778baf408c730652822dad314bbb04181e3d5d92
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-04-08 20:39:14 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,49 @@
+public class Pessoa{
+	int cpf = 0;
+	String nome = "";
+
+
+	public static void main (String args[]){
+		Pessoa p1 = new Pessoa();
+		p1.setCpf(12);
+		p1.setNome("rafael");		
+
+
+		Pessoa p2=new Pessoa();
+		p2.setCpf(13);
+		p2.setNome("nicolly");	
+
+
+		System.out.println("Endereco de p1:... "+p1);
+		System.out.println("Endereco de p2:... "+p2);
+
+
+		System.out.println("Nome de p1:... "+p1.nome);
+		System.out.println("CPF de p1:... "+p1.cpf);
+		System.out.println("Nome de p2:... "+p2.nome);
+		System.out.println("CPF de p2:... "+p2.cpf);
+
+
+		System.gc();
+		
+	}
+
+	public int getCpf(){
+  
+		return cpf;
+	
+}
+	public String getNome(){
+  
+		return nome;
+	
+}
+	public void setCpf(int cpf){
+  
+		this.cpf = cpf; 
+	}
+	public void setNome(String nome){
+  
+		this.nome = nome; 
+	}
+}
