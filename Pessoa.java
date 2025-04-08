f8dbaef5e50e2ec0b62d1d83e7e277c22cf4713e
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-04-08 20:45:28 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,33 +1,7 @@
 public class Pessoa{
 	int cpf = 0;
 	String nome = "";
-
-
-	public static void main (String args[]){
-		Pessoa p1 = new Pessoa();
-		p1.setCpf(12);
-		p1.setNome("rafael");		
-
-
-		Pessoa p2=new Pessoa();
-		p2.setCpf(13);
-		p2.setNome("nicolly");	
-
-
-		System.out.println("Endereco de p1:... "+p1);
-		System.out.println("Endereco de p2:... "+p2);
-
-
-		System.out.println("Nome de p1:... "+p1.nome);
-		System.out.println("CPF de p1:... "+p1.cpf);
-		System.out.println("Nome de p2:... "+p2.nome);
-		System.out.println("CPF de p2:... "+p2.cpf);
-
-
-		System.gc();
-		
-	}
-
+	
 	public int getCpf(){
   
 		return cpf;
