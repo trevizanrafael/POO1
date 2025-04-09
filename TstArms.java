e37ea9e0220b12dc02a55e02f1885820941d6a2a
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-04-09 14:10:35 -0300

    Update TstArms.java

--- TstArms.java
+++ TstArms.java
@@ -3,11 +3,14 @@ public class TstArms{
 		Armas a1;
 		Armas a2;
 		
+		a1 = new Armas();
+		a2 = new Armas();
+
 		a1.setNome("Ak-47");
 		a2.setNome("M4-A1");
 		
-		a1.setFabricante("Ak-47");
-		a2.setFabricante("M4-A1");
+		a1.setFabricante("Russia");
+		a2.setFabricante("EUA");
 		
 		a1.setAno_c(1947);
 		a2.setAno_c(1983);
@@ -15,8 +18,8 @@ public class TstArms{
 		a1.setMunicao(30);
 		a2.setMunicao(50);
 		
-		System.out.println(a1.nome+a1.fabricante+a1.ano_c+a1.municao);
-		System.out.println(a2.nome+a2.fabricante+a2.ano_c+a2.municao);
+		System.out.println(a1.nome+" "+a1.fabricante+" "+a1.ano_c+" "+a1.municao);
+		System.out.println(a2.nome+" "+a2.fabricante+" "+a2.ano_c+" "+a2.municao);
 		
 	}
 	
