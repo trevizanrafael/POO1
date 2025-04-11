8296916c825b50fb892103387855cfb8334c50cb
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-04-10 22:02:33 -0300

    Update Armas.java

--- Armas.java
+++ Armas.java
@@ -1,14 +1,9 @@
 public class Armas{
 	
 	private String nome = "";
-	
 	private int municao = 0;
-	
 	private int ano_c = 0;
-	
 	private String fabricante = "";
-	
-
 	private Endereco lugar = new Endereco();
 	
 
@@ -16,7 +11,8 @@ public class Armas{
 	public Endereco getLugar(){
 		return lugar;
 	}
-	public Endereco setLugar(Endereco lugar){
+
+	public void setLugar(Endereco lugar){
 		this.lugar=lugar;
 	}
 	
@@ -24,58 +20,39 @@ public class Armas{
 	
 	
 	public int getMunicao(){
-		
 		return municao;
-	
 	}
 	
 	public int getAno_c(){
-		
 		return ano_c;
-	
 	}
 	
 	public String getNome(){
-		
 		return nome;
-	
 	}
 	
 	public String getFabricante(){
-		
 		return fabricante;
-	
 	}
 	
 	
 	public void setMunicao(int municao){
 		if(municao>0){
-		
 			this.municao=municao;
-	
 		} else {
 			System.out.println("ERRO NO CADASTRO DE MUNICAO");
 		}
 	}
 	
 	public void setAno_c(int ano_c){
-		
 		this.ano_c=ano_c;
-	
 	}
 	
 	public void setNome(String nome){
-		
 		this.nome=nome;
-	
 	}
 	
 	public void setFabricante(String fabricante){
-		
 		this.fabricante=fabricante;
-	
 	}
-
-		
-
 }
