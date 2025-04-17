f88aa8cf5578aeeca55c85626c47d44297efce9a
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-04-17 20:39:30 -0300

    Create Cidade.java

---
+++ Cidade.java
@@ -0,0 +1,38 @@
+public class Cidade{
+	private int codCid;
+	private String nomeCid;
+	private String ufCid;
+	
+	public Cidade(){
+		System.out.println("Construtor de Cidades");
+		
+	}
+
+	public void setCodCid(int codCid){
+		if(codCid>0){
+			this.codCid=codCid;
+		} else {
+			System.out.println("ERRO NO CADASTRO DE CEP");
+		}
+	}
+	
+	public void setNomeCid(int nomeCid){
+		this.nomeCid=nomeCid;
+	}
+	
+	public void setUfCid(String ufCid){
+		this.ufCid=ufCid;
+	}
+
+	public int getCodCid(){
+		return CodCid;
+	}
+	
+	public String getNomeCid(){
+		return nomeCid;
+	}
+	
+	public String getUfCid(){
+		return ufCid;
+	}
+}
