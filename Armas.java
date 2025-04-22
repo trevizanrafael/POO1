8e0f3c98cdcc1fde7f20cfd0a6796939dccc7cd7
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-04-21 23:15:30 -0300

    adição de todas as classes

---
+++ Armas.java
@@ -0,0 +1,60 @@
+public class Armas{
+	
+	private String nome = "";
+	private int municao = 0;
+	private int ano_c = 0;
+	private String fabricante = "";
+	private Endereco lugar = new Endereco();
+	
+	
+	public Armas(){
+		System.out.println("Construtor de armas");
+		municao = 30;
+		nome = "AK-47";
+	}
+
+	public Endereco getLugar(){
+		return lugar;
+	}
+
+	public void setLugar(Endereco lugar){
+		this.lugar=lugar;
+	}
+	
+	public int getMunicao(){
+		return municao;
+	}
+	
+	public int getAno_c(){
+		return ano_c;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public String getFabricante(){
+		return fabricante;
+	}
+	
+	
+	public void setMunicao(int municao){
+		if(municao>0){
+			this.municao=municao;
+		} else {
+			System.out.println("ERRO NO CADASTRO DE MUNICAO");
+		}
+	}
+	
+	public void setAno_c(int ano_c){
+		this.ano_c=ano_c;
+	}
+	
+	public void setNome(String nome){
+		this.nome=nome;
+	}
+	
+	public void setFabricante(String fabricante){
+		this.fabricante=fabricante;
+	}
+}
---
+++ TstArms.java
@@ -0,0 +1,94 @@
+public class TstArms{
+	public static void main(String args[]){
+		//Criacao dos objetos
+        Armas a1;
+        Leitura l;
+
+        //Inicializacao dos objetos
+		a1 = new Armas();
+        l =  new Leitura();
+
+        //Entrada do nome do objeto
+		String nome = l.entDados("Nome da arma:");
+		a1.setNome(nome);	
+
+		
+		//Entrada do nome do fabricante
+		String fabricante = l.entDados("Nome do fabricante da arma:");
+		a1.setFabricante(fabricante);
+		
+		//Entrada do ano de criacao da arma
+        boolean ver;
+        do{    
+            ver = true;
+            String ano = l.entDados("Ano de criacao da arma:");
+            try{
+                a1.setAno_c(Integer.parseInt(ano));
+            } catch(NumberFormatException e){
+                System.out.println("Digite apenas numerais.");
+                ver = false;
+            }
+        } while(!ver);
+
+		//Entrada do numero de balas
+        do{    
+            ver = true;
+            String municao = l.entDados("Numero de balas da arma:");
+            try{
+                a1.setMunicao(Integer.parseInt(municao));
+            } catch(NumberFormatException e){
+                System.out.println("Digite apenas numerais.");
+                ver = false;
+            }
+        } while(!ver);
+
+        do{
+            String codCid = l.entDados("CEP da cidade que esta a arma:");
+            ver = true;
+            try{
+                a1.getLugar().getCidade().setCodCid(Integer.parseInt(codCid));
+            } catch(NumberFormatException e){
+                System.out.println("Digite apenas numerais.");
+                ver = false;
+            }
+        } while(!ver);
+
+        String nomeCid = l.entDados("Cidade que esta a arma:");
+        a1.getLugar().getCidade().setNomeCid(nomeCid);
+
+        String nomeUF = l.entDados("Estado que esta a arma:");
+        a1.getLugar().getCidade().setUfCid(nomeUF);
+
+
+
+        String rua = l.entDados("Rua da casa da arma:");  
+		a1.getLugar().setRua(rua);
+
+        do{
+            String numero = l.entDados("Numero da casa da arma:");
+            ver = true;
+            try{
+                a1.getLugar().setNumero(Integer.parseInt(numero));
+            } catch(NumberFormatException e){
+                System.out.println("Digite apenas numerais.");
+                ver = false;
+            }
+        } while(!ver);
+
+        String nome1 = a1.getNome();
+		String fabricante1 = a1.getFabricante();
+		int municao1 = a1.getMunicao();
+		int ano1 = a1.getAno_c();
+		String rua1 = a1.getLugar().getRua();
+		int num1 = a1.getLugar().getNumero();
+        int cep = a1.getLugar().getCidade().getCodCid();
+        String nomcid = a1.getLugar().getCidade().getNomeCid();
+        String estcid = a1.getLugar().getCidade().getUfCid();
+
+		
+
+
+
+		System.out.println(nome1+" "+fabricante1+" "+municao1+" "+ano1+"\n"+rua1+" "+num1+"\n"+cep+" "+nomcid+" "+estcid);
+	}
+}
